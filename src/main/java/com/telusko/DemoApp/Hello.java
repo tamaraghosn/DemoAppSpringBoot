a0aacfc92762d10334621d0817960f32c19cc4d6
tamaraghosn <tamaraghosn01@gmail.com>
2025-09-15 16:59:17 +0300

    added Hello Tamara feature

--- src/main/java/com/telusko/DemoApp/Hello.java
+++ src/main/java/com/telusko/DemoApp/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 
     @RequestMapping("/")
     public String greet(){
-        return "Hello World, Welcome to Telusko";
+        return "Hello World, Welcome to Tamara Ghosn";
     }
 
 }
