1b6866afd9ced13de4d2f9da2cfab2cdfca01255
tamaraghosn <tamaraghosn01@gmail.com>
2025-09-15 18:13:47 +0300

    changing the hello message

--- src/main/java/com/telusko/DemoApp/Hello.java
+++ src/main/java/com/telusko/DemoApp/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 
     @RequestMapping("/")
     public String greet(){
-        return "Hello World, Welcome to Tamara Ghosn";
+        return "Hello World, Welcome to Tamara Ghosn and let's get started";
     }
 
 }
